e8e27b978247080a8273d56df0c23e8a31419421
Saito_Karuha <3250884188@qq.com>
2024-11-16 19:05:42 +0800

    Changed the ActorNet into the one which we could do bp algorithm through the logits

--- PRM_DDPG.py
+++ PRM_DDPG.py
@@ -35,11 +35,13 @@ class CriticNet(torch.nn.Module):
         x = self.v_head_mlp3(x) # shape:(句子数,1)
         return x
 
+
 class ActionNet(torch.nn.Module):
     '''这里选用模型 peiyi9979/mistral-7b-sft, 输出严格遵循确定的格式, 格式如下:
     Step 1: <content> ки\nStep 2 ,,,以此类推; 注意<content>中可以包含\n, 我们真正需要的 step-tag是 'ки\n'
     (但是还是建议加上 verifier防止训练时炸掉)'''
-    def __init__(self, base_model:str, cache_dir:str, chat_template:str):
+
+    def __init__(self, base_model: str, cache_dir: str, chat_template: str):
         super().__init__()
         self.base_model = AutoModelForCausalLM.from_pretrained(base_model, cache_dir=cache_dir)
 
@@ -56,9 +58,10 @@ class ActionNet(torch.nn.Module):
                                                        cache_dir=cache_dir)
         self.chat_template = chat_template
         self.special_tag = 'ки'
-        self.special_tag_id = self.tokenizer.encode(self.special_tag)[1] #12902
+        self.special_tag_id = self.tokenizer.encode(self.special_tag)[1]  # 12902
 
-    def forward(self, question:str=None, pre_reasoning:str=None, num_gen:int=1):
+    def forward(self, question: str = None, pre_reasoning: str = None, num_gen: int = 1,
+                if_return_logits: bool = False):
         '''前向推理函数，生成下一步的推理
         (question, pre_reasoning)都是字符串, 即自然语言形式表述的问题以及前述推理步骤, num_gen表示生成数
         返回值, 'response_ids':下一步推理的 id_tensor:[tensor1,tensor2,...], 'nl_response':[str1,str2,...]
@@ -70,23 +73,36 @@ class ActionNet(torch.nn.Module):
             ]
         else:
             messages = [
-                {"role": "user", "content": question+"\nSolution:\n"+pre_reasoning}
+                {"role": "user", "content": question + "\nSolution:\n" + pre_reasoning}
             ]
         inputs = self.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
         model_inputs = self.tokenizer([inputs], return_tensors='pt')
         generated_ids = self.base_model.generate(
             **model_inputs,
-            temperature = 0.4,
-            do_sample = True,
+            temperature=0.4,
+            do_sample=True,
             num_return_sequences=num_gen,
             max_new_tokens=512,
             return_dict_in_generate=True,
-            eos_token_id = [self.tokenizer.eos_token_id, self.special_tag_id]
-        ) # 会生成一个列表, 大小是[num_gen,文本长度+Pad]
+            eos_token_id=[self.tokenizer.eos_token_id, self.special_tag_id]
+        )  # 会生成一个列表, 大小是[num_gen,文本长度+Pad]
         generated_ids = generated_ids.sequences
+        generated_ids.detach()  # 分离计算图
         next_steps = []
         a = len(model_inputs.input_ids[0])
+        logits = []
         for i in range(len(generated_ids)):
             next_steps.append(generated_ids[i][a:])
+            if if_return_logits:
+                # 把输出的答案+问题+prompt重新输入LM获取Logits的部分 --- shape(答案长度,vocab)
+                each_logits = self.base_model(input_ids=generated_ids[i].unsqueeze(0),
+                                              attention_mask=torch.ones_like(generated_ids[i].unsqueeze(0)),
+                                              return_dict=True).logits[0][a - 1:-1, :]
+                each_logits = torch.nn.functional.softmax(each_logits, dim=-1)
+                logits.append(
+                    torch.gather(each_logits, dim=-1, index=generated_ids[i][a:].reshape(-1, 1)).reshape(1, -1))
+                # logits中每个元素都是shape为(1,回答序列长度)的torch.tensor
+
         nl_response = self.tokenizer.batch_decode(next_steps)
-        return {'response_ids': next_steps, 'nl_response': nl_response, 'actual_numGen': len(next_steps)}
+        return {'response_ids': next_steps, 'nl_response': nl_response, 'actual_numGen': len(next_steps),
+                'correspond_logits': logits}
